b6091f42d82538bcf729229f37f490b325b9b15d
Gauri Jawalekar <gauri.jawalekar@gmail.com>
2023-09-14 12:01:09 +0100

    test Commit

---
+++ src/test/java/SeleniumPractice/Login.java
@@ -0,0 +1,20 @@
+package SeleniumPractice;
+import org.testng.annotations.Test;
+
+public class Login {
+
+    @Test
+    public void cloginTest() {
+        System.out.println("Login successful");
+    }
+
+    @Test
+    public void bregisterTest() {
+        System.out.println("Register successful");
+    }
+
+    @Test
+    public void alogoutTest() {
+        System.out.println("Logout successful");
+    }
+}
